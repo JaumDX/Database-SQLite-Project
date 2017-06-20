c9a8d34b476d81e97ecd1170c455c2df71f06142
Joao <joaovitornaru@hotmail.com>
2017-06-20 17:49:56 -0300

    Arrumando navigation.

--- DatabaseSQLiteProject/AddViewController.swift
+++ DatabaseSQLiteProject/AddViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class AddViewController: UIViewController {
+    
+    
+    //MARK: Outlets
+    
+    @IBOutlet weak var nomeTarefa: UITextField!
+    @IBOutlet weak var descricaoTarefa: UITextView!
+    @IBOutlet weak var dataLimite: UIDatePicker!
+    @IBOutlet weak var cadastrarButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupVisual()   // Setup borders to some fields.
+        dataLimite.minimumDate = Date()  // Set the actual date/time as minimum.
+        
     }
+    
+    
+    
+    //MARK: Setup Visual
+    
+    func setupVisual(){
+        
+        self.descricaoTarefa.layer.borderWidth = 1
+        self.dataLimite.layer.borderWidth = 1
+        self.cadastrarButton.layer.borderWidth = 1
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
