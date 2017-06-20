accb5cd8bd0c7d8b49bae8d69a61b32fd8f41106
Joao <joaovitornaru@hotmail.com>
2017-06-20 01:09:21 -0300

    Criado funcoes para conectar ao banco e criar uma table.

--- DatabaseSQLiteProject/TabelaDAO.swift
+++ DatabaseSQLiteProject/TabelaDAO.swift
@@ -9,22 +9,65 @@
 import Foundation
 import SQLite
 
-class TabelaDAO: NSObject {
+class TabelaDAO {
     
-    override init() {
+    static let shared : TabelaDAO = TabelaDAO() //Singleton
+    
+    private let db: Connection?   //Database Conection.
+    
+    
+    //Database Expressions.
+    
+    private let contacts = Table("contacts")
+    private let id = Expression<Int64>("id") //NomeDaConstante = Expressao<Tipo>("NomeDaExpressao")
+    private let descricao = Expression<String>("descricao")
+    
+    
+    private init(){
         
+        
+        //Search for the path of database.
         let path = NSSearchPathForDirectoriesInDomains(
             .documentDirectory, .userDomainMask, true
             ).first!
         
-       // _ = try Connection("\(path)/db.sqlite3")
+        
+        //Try to do a connection.
+        do {
+            db = try Connection("\(path)/db.sqlite3")
+        } catch  {
+            db = nil
+            print("Unable to conect the database.")
+        }
+        
+        createTable() //Chama a funçao de criar a table
+        
         
     }
+
+    //Function to create a table
     
-    
-    
-    
-    
+    func createTable(){
+        
+        /*
+ 
+         If a table doesn't exists, it will create a new table and insert the following collumns. Each collumn initializate with a expression.
+         
+         */
+        
+        
+        do {
+            try db!.run(contacts.create(ifNotExists: true) { table in
+                table.column(id, primaryKey: true)
+                table.column(descricao)
+                
+                })
+        } catch  {
+            print("Unable to create a table.")
+        }
+        
+        
+    }
     
     
 }
